2876960d91cc997adad8d8ee81a3e930b3b9fde7
Max Wiens-Evangelista <rothmoss@gmail.com>
2018-10-20 16:26:25 -0500

    working on getting the ball to bounce

--- src/systems/ball.rs
+++ src/systems/ball.rs
@@ -1,7 +1,6 @@
 use amethyst::core::transform::components::Transform;
 use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
 use pong::{Paddle, Ball, ARENA_HEIGHT, ARENA_WIDTH, BALL_RADIUS};
-
 pub struct BallSystem;
 
 impl<'s> System<'s> for BallSystem {
@@ -17,22 +16,23 @@ impl<'s> System<'s> for BallSystem {
 	);
 
 	fn run(&mut self, (mut transforms, paddles, balls): Self::SystemData) {
+		let mut dx : f32 = 0.0;
+		let mut dy : f32 = 0.0;
+		let mut new_angle : Option<f32> = None;
 		for (ball, transform) in (&balls, &mut transforms).join() {
-
-
-			let dx = (transform.translation.x + ball.velocity.x)
+			dx = (transform.translation.x + ball.velocity.x)
 				.min(ARENA_WIDTH - BALL_RADIUS)
 				.max(BALL_RADIUS);
-			let dy = (transform.translation.y + ball.velocity.y)
+			dy = (transform.translation.y + ball.velocity.y)
 				.min(ARENA_HEIGHT - BALL_RADIUS)
 				.max(BALL_RADIUS);
-
-			for (paddles, transform) in  ().join() {
-
-
-			}
-
-
+		}
+		for (paddle, transform) in  (&paddles, &transforms).join() {
+			// if is colliding
+			new_angle = Some((dy/dx).atan());
+		}
+		if let Some(angle) = new_angle {
+			angle
 		}
 	}
 }
